b606ca09c176e6ec5ab43b9991f88ddb8cfa3c8f
Blightwidow <theo@dammaretz.fr>
2020-12-23 16:29:33 +0100

    feat(upgrade): Add token to upgrades

--- src/handlers/simulation.js
+++ src/handlers/simulation.js
@@ -7,6 +7,52 @@ import * as raidbotsProvider from "../providers/raidbots.js";
 
 const raidbotQueue = new PQueue.default({ concurrency: 1 });
 
+function mapWeaponToToken(upgrade) {
+  const MAPPING = {
+    183892: [182422, 182418, 182424, 182417, 182419],
+    183897: [182392, 182396, 182398, 182391, 182393],
+    183893: [182421, 182415, 182422, 182418, 182416, 182420, 182423],
+    183896: [182395, 182389, 182392, 182396, 182390, 182394, 182397],
+    183888: [182425, 182426],
+    183895: [182399, 182400],
+    183891: [182417, 182422, 182421, 182415, 182418, 182416],
+    183898: [182391, 182396, 182395, 182389, 182392, 182390],
+    183889: [182425, 182426],
+    183894: [182399, 182400],
+    183890: [
+      182416,
+      182420,
+      182419,
+      182417,
+      182422,
+      182418,
+      182415,
+      182421,
+      182414,
+    ],
+    183899: [
+      182390,
+      182394,
+      182393,
+      182391,
+      182396,
+      182392,
+      182395,
+      182389,
+      182388,
+    ],
+  };
+  const nameParts = upgrade.name.split("/");
+  const initialId = Number.parseInt(nameParts[3]);
+
+  const nextItemId = Object.entries(MAPPING).reduce(
+    (acc, [key, value]) => (value.includes(initialId) ? key : acc),
+    initialId
+  );
+
+  return { ...upgrade, itemID: nextItemId };
+}
+
 async function generateSim(charName) {
   try {
     logger.debug("[Simulation] ", `Starting new sim for ${charName}`);
@@ -68,16 +114,19 @@ async function updateSimReport(reportID) {
     }, {});
 
     await Promise.all(
-      Object.values(upgrades).map((result) =>
-        databaseProvider.upsertUpgrade(
-          user.id,
-          result,
-          report.sim.players[0].collected_data.dps,
-          reportID,
-          report.sim.players[0].specialization,
-          report.simbot.date
+      Object.values(upgrades)
+        .map(mapWeaponToToken)
+        .map((result) =>
+          databaseProvider.upsertUpgrade(
+            user.id,
+            result.itemID,
+            result,
+            report.sim.players[0].collected_data.dps,
+            reportID,
+            report.sim.players[0].specialization,
+            report.simbot.date
+          )
         )
-      )
     );
   } catch (error) {
     logger.error("[Simulation] ", error);
