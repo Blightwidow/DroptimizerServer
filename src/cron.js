0f0475850231ce68b1288b5ca9ca6e8fbe23b561
Theo Dammaretz <theo@dammaretz.fr>
2021-01-27 10:36:25 +0100

    fix(cron): Avoid memory leak on cron handler

--- src/cron.js
+++ src/cron.js
@@ -7,7 +7,6 @@ import { updateItems } from "./handlers/items.js";
 export async function initData() {
   try {
     await updateItems();
-    await queueAllSims();
   } catch (e) {
     logger.error("[Data] ", e);
   }
@@ -17,22 +16,20 @@ export function initCrons() {
   // start new droptimizer sims at 5:00am every day
   cron.schedule(
     "0 5 * * *",
-    function () {
+    () => {
       logger.warn("[CRON] ", "Running character sims");
 
       queueAllSims();
     },
-    { timezone: process.env.TIMEZONE }
   );
 
   // update items at 4:00am every day
   cron.schedule(
     "0 0 1 * *",
-    function () {
+    () => {
       logger.warn("[CRON] ", "Updating items");
 
       updateItems();
     },
-    { timezone: process.env.TIMEZONE }
   );
 }
